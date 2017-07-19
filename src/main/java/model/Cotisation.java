935c87d3e1d2f8534402a7c0d574a2327a635e26
Nouar <nouar.abderahmane@hotmail.com>
2017-07-19 11:35:43 +0200

    mettre les entites avec les sequences

--- src/main/java/model/Cotisation.java
+++ src/main/java/model/Cotisation.java
@@ -1,5 +1,55 @@
 package model;
 
-public class Cotisation {
+import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+@Entity
+@Table (name="cotisation")
+@SequenceGenerator(name="seq_cotisation",sequenceName="seq_cotisation",initialValue=1,allocationSize=1)
+public class Cotisation {
+    @Id
+    @GeneratedValue(generator="seq_cotisation")
+	private Long id;
+    @Column
+	private Long montant;
+    @Column
+	private LocalDate datePaiment;
+
+	public Cotisation(Long montant, LocalDate datePaiment) {
+		this.montant = montant;
+		this.datePaiment = datePaiment;
+	}
+
+	public Cotisation() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Long getMontant() {
+		return montant;
+	}
+
+	public LocalDate getDatePaiment() {
+		return datePaiment;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setMontant(Long montant) {
+		this.montant = montant;
+	}
+
+	public void setDatePaiment(LocalDate datePaiment) {
+		this.datePaiment = datePaiment;
+	}
 }
--- src/main/java/model/Media.java
+++ src/main/java/model/Media.java
@@ -1,5 +1,65 @@
 package model;
 
-public class Media {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
+@Entity
+@Table (name="media")
+@SequenceGenerator(name="seq_media",sequenceName="seq_media",initialValue=1,allocationSize=1)
+public class Media {
+    @Id
+    @GeneratedValue(generator="seq_media")
+	private Long id;
+	@Column
+	private String titre;
+	@Column
+	private String auteur;
+	@Column
+	private TypeMedia media;
+
+	public Media(String titre, String auteur, TypeMedia media) {
+		this.titre = titre;
+		this.auteur = auteur;
+		this.media = media;
+	}
+
+	public Media() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public String getAuteur() {
+		return auteur;
+	}
+
+	public TypeMedia getMedia() {
+		return media;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+
+	public void setMedia(TypeMedia media) {
+		this.media = media;
+	}
 }
