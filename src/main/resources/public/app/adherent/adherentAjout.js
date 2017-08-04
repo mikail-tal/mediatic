a0a7a2050419839368549e8e10c0e0d20996ec10
AYOUB BENGHAL <ayoub.benghal@gmail.com>
2017-08-04 14:59:08 +0200

    MODEL Adherent

--- src/main/resources/public/app/adherent/adherentAjout.js
+++ src/main/resources/public/app/adherent/adherentAjout.js
@@ -4,5 +4,13 @@ angular.module('myApp')
     .controller('AdherentAjoutCtrl', ['$scope', function ($scope) {
 
         $scope.hello="HELLO WORLD ADHERENT AJOUT";
+        $scope.adherentsTab=[];
+
+
+        $scope.ajouterAdherent=function(){
+
+
+            
+        }
 
     }]);
